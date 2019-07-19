8bf62c72bd21c5fedd1afb7cae27c44a8ba69191
Matthias <matthiasduyck@gmail.com>
2019-07-20 01:20:05 +0200

    Rework qr wifi string parsing

--- Wifi QR code scanner/Business/WifiAccessPointData.cs
+++ Wifi QR code scanner/Business/WifiAccessPointData.cs
@@ -4,9 +4,18 @@
     {
         public string ssid { get; set; }
         public string password { get; set; }
+
+        public WifiAccessPointSecurity wifiAccessPointSecurity {get;set;}
+        
         public override string ToString()
         {
             return "Network Name (SSID): " + ssid + System.Environment.NewLine +  "Password: " + password;
         }
+    }
+    public enum WifiAccessPointSecurity
+    {
+        WEP,
+        WPA,
+        nopass
     }
 }
